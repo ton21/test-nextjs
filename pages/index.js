9445b352d8d8b3cf563c0cc42e5f45b49c01964f
Wilton Santos <wilton.stos@gmail.com>
2020-11-18 16:32:15 -0300

    update

--- pages/index.js
+++ pages/index.js
@@ -3,22 +3,7 @@ import { useState } from 'react';
 const Home = () => {
   return (
     <div>
-      <h1>Home</h1>
-      <p>lorem ipusm</p>
-      <Contador />
-    </div>
-  );
-};
-
-const Contador = () => {
-  const [counter, seCounter] = useState(0);
-  const handleCounter = () => {
-    seCounter(counter + 1);
-  };
-  return (
-    <div>
-      <div>{counter}</div>
-      <button onClick={handleCounter}>Increment</button>
+      <h2>Home</h2>
     </div>
   );
 };
